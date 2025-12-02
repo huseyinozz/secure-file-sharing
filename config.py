060d32554ef92ab4efc43b6796f1ce59bc15f35c
MuhammedCaglar <96307546+SeemsOdd@users.noreply.github.com>
2025-12-02 11:57:52 +0300

    Feat: Config dosyası ve Storage Bucket ayarları eklendi

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    # Rapor 2: Config ayarlari ve Bucket tanimlamasi
+    # Guvenlik icin SECRET_KEY ortam degiskeninden alinmali
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'guvenli-bir-anahtar-varsayilan'
+    
+    # Firebase Storage Bucket Adi
+    STORAGE_BUCKET = 'file-sharing-master-fbccc.appspot.com'
