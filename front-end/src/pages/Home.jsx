565b31cc07e3eb40ad08a0177648cd82d1de819e
huseyinfidanfid <huseyinfidanfid@gmail.com>
2025-12-02 15:09:09 +0300

    Update Home.jsx

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,4 +1,12 @@
+import React from 'react';
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <h1>Hoşgeldiniz</h1>
+      <p>Dosyalarınızı şifreleyin, güvenle paylaşın.</p>
+    </div>
+  );
 };
+
 export default Home;
