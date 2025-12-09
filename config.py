6a4140608d676f4406681370d8cd33dc1ce5b602
Hüseyin Öz <huseyinoz@neokodyazilim.com>
2025-12-09 23:32:10 +0300

    Max content length control added

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     # Guvenlik icin SECRET_KEY ortam degiskeninden alinmali
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'guvenli-bir-anahtar-varsayilan'
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # 20 MB
 
     
     # Firebase Storage Bucket Adi
