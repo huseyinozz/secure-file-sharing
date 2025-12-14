5f802f08e250c2163f4a5d1e0e6de6029e3ae1e1
huseyinfidanfid <huseyinfidanfid@gmail.com>
2025-12-14 19:16:59 +0300

    Update Navbar.jsx

--- front-end/src/Navbar.jsx
+++ front-end/src/Navbar.jsx
@@ -3,17 +3,13 @@ import { Link } from "react-router-dom";
 import "./App.css";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <Link to="/" className="nav-link">
-        Anasayfa
-      </Link>
-      <Link to="/upload" className="nav-link">
-        Dosya Yükle
-      </Link>
-      <Link to="/download" className="nav-link">
-        Dosya İndir
-      </Link>
+ return (
+    <nav style={{ background: '#333', padding: '15px', color: 'white' }}>
+      <ul style={{ display: 'flex', listStyle: 'none', gap: '20px', margin: 0 }}>
+        <li><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Ana Sayfa</Link></li>
+        <li><Link to="/upload" style={{ color: 'white', textDecoration: 'none' }}>Dosya Yükle</Link></li>
+        <li><Link to="/download" style={{ color: 'white', textDecoration: 'none' }}>Dosya İndir</Link></li>
+      </ul>
     </nav>
   );
 };
